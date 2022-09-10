d6589c6edba1f0fcc46ef0c4298689768503330b
Irfan Hanif <14.irfanh@gmail.com>
2022-09-10 08:44:37 +0700

    added folder and file to fix issue

---
+++ Irfan Hanif H/bab3/Answer/latihan5.js
@@ -0,0 +1,7 @@
+// Latihan 5 Bab 3
+
+console.log(2000 + 500);
+console.log(2000 - 500);
+console.log(2000 * 500);
+console.log(2000 / 500);
+console.log(2000 % 500);
---
+++ Irfan Hanif H/tambahan1_github/fitur-pertama.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Halaman HTML</h1>
+        
+        <?php
+            echo('Hello World!');
+        ?>
+    </body>
+</html>
